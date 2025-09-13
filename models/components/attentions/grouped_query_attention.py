bd3a287e3cbfdcbee9f41c0ac24637b2e8d25515
Ansh <eye.am.ansh@gmail.com>
2025-09-12 21:58:25 -0700

    Make changes to the attention module to integrate

--- models/components/attentions/grouped_query_attention.py
+++ models/components/attentions/grouped_query_attention.py
@@ -11,7 +11,7 @@ from models.components.attentions.utils import get_default_causal_mask
 import math
 
 class GroupedQueryAttention(nn.Module):
-    def __init__(self, num_query_heads=8, num_kv_heads = 8, embedding_dimension=1024, head_dimension = 512):
+    def __init__(self, num_query_heads=8, num_kv_heads = 8, embedding_dimension=1024, head_dimension = 512, **kwargs):
         super().__init__()
         self.embedding_dimension = embedding_dimension
         self.head_dimension = head_dimension
